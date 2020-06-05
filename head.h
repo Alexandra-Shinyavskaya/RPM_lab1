768dd82abbe00e1d191bb0120957192b88105b7c
Alexandra-Shinyavskaya <66488076+Alexandra-Shinyavskaya@users.noreply.github.com>
2020-06-05 20:47:58 +0300

    Create head.h

---
+++ head.h
@@ -0,0 +1,20 @@
+/**
+ * @file obs.h
+ * @author Shinyavskaya Alexandra <alexsa-shinavskaya@mail.ru>
+ * @brief Содержит описание
+ */
+#ifndef head
+#define head
+#include <iostream>
+#include <string>
+#define n 10
+class Subject;
+class Road;
+/**
+* Функция вывода на экран
+* @param av существующие объекты
+* @param ObjectRoad[n] объекты
+* @param svetofor отслеживаемый объект
+*/
+void mapaful( int av[n], Road* ObjectRoad[n], Subject* svetofor );
+#endif
